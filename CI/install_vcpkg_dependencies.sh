f4f5f229d0661280cc3500951d3d252bf5cc1620
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2025-07-14 14:50:44 +0200

    Update install_vcpkg_dependencies.sh

--- CI/install_vcpkg_dependencies.sh
+++ CI/install_vcpkg_dependencies.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-RELEASE_TAG="v1.8"
 FILENAME="dependencies-$1"
+
+# Fetch latest release tag dynamically
+RELEASE_TAG=$(curl -s https://api.github.com/repos/vcmi/vcmi-deps-windows/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+
 DOWNLOAD_URL="https://github.com/vcmi/vcmi-deps-windows/releases/download/$RELEASE_TAG/$FILENAME.txz"
 
 curl -L "$DOWNLOAD_URL" | tar -xf - --xz
