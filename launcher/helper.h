cce19dcb5753e822bbe08b571a572c5400ac024d
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2025-10-10 14:07:59 +0200

    Update helper.h

--- launcher/helper.h
+++ launcher/helper.h
@@ -16,12 +16,13 @@ class MainWindow;
 
 namespace Helper
 {
-void loadSettings();
-void reLoadSettings();
-void enableScrollBySwiping(QObject * scrollTarget);
-QString getRealPath(QString path);
-bool performNativeCopy(QString src, QString dst);
-void revealDirectoryInFileBrowser(QString path);
-MainWindow * getMainWindow();
-void keepScreenOn(bool isEnabled);
+	void loadSettings();
+	void reLoadSettings();
+	void enableScrollBySwiping(QObject * scrollTarget);
+	QString getRealPath(QString path);
+	bool performNativeCopy(QString src, QString dst);
+	void revealDirectoryInFileBrowser(QString path);
+	MainWindow * getMainWindow();
+	void keepScreenOn(bool isEnabled);
+	void nativeFolderPicker(QWidget *parent, std::function<void(QString)> cb);
 }
