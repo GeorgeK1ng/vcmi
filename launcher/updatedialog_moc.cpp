be4a1a6e354d6d485c365c70f1c5be61fe42226d
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2025-10-19 21:45:43 +0200

    Update updatedialog_moc.cpp

--- launcher/updatedialog_moc.cpp
+++ launcher/updatedialog_moc.cpp
@@ -13,7 +13,7 @@
 
 #include "../lib/CConfigHandler.h"
 #include "../lib/GameConstants.h"
-#include "../lib/Version.h"
+#include "../Version.h"
 
 #include <QNetworkReply>
 #include <QNetworkRequest>
