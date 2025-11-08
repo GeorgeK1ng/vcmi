3198173eab54f3fbbb85a8912f672133b8fd15e0
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2025-11-08 17:53:07 +0100

    Add files via upload

--- android/vcmi-app/src/main/java/eu/vcmi/vcmi/util/FileUtil.java
+++ android/vcmi-app/src/main/java/eu/vcmi/vcmi/util/FileUtil.java
@@ -5,6 +5,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.ParcelFileDescriptor;
 import android.provider.OpenableColumns;
 import android.provider.DocumentsContract;
 
