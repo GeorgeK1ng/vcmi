18db90f9498d2a8c094373d40f82bc69043b4f26
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2026-02-17 13:33:52 +0100

    Restore centered update title and style round help button

--- launcher/updatedialog_moc.cpp
+++ launcher/updatedialog_moc.cpp
@@ -113,11 +113,11 @@ UpdateDialog::UpdateDialog(bool calledManually, QWidget *parent):
 	if(ui->infoButton)
 	{
 		ui->infoButton->setIcon(style()->standardIcon(QStyle::SP_DialogHelpButton));
-		ui->infoButton->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
-		ui->infoButton->setText(tr("?"));
-		QFont infoFont = ui->infoButton->font();
-		infoFont.setBold(true);
-		ui->infoButton->setFont(infoFont);
+		ui->infoButton->setToolButtonStyle(Qt::ToolButtonIconOnly);
+		ui->infoButton->setText(QString());
+		ui->infoButton->setCursor(Qt::PointingHandCursor);
+		ui->infoButton->setStyleSheet("QToolButton#infoButton { border: 1px solid palette(mid); border-radius: 14px; padding: 3px; background: palette(button); } QToolButton#infoButton:hover { background: palette(light); }");
+		ui->infoButton->raise();
 	}
 
 	if(calledManually)
