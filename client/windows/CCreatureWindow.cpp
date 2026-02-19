61ba478a602b5def64ca2338f62d74f2a1c3650a
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2026-02-19 08:49:58 +0100

    Update hover text and icon description handling

--- client/windows/CCreatureWindow.cpp
+++ client/windows/CCreatureWindow.cpp
@@ -524,15 +524,20 @@ CStackWindow::CommanderMainSection::CommanderMainSection(CStackWindow * owner, i
 					{
 						parent->setSelection(skillID, icon);
 					};
+					std::string abilityDescription;
 					for(int i = 0; i < bonuses.size(); i++) 
 					{
 						icon->hoverText += stack->bonusToString(bonuses[i]);
+						if(!abilityDescription.empty())
+							abilityDescription += "\n";
+
+						abilityDescription += stack->bonusToString(bonuses[i]);
 					}
+					icon->hoverText = abilityDescription;
+					icon->text = abilityDescription;
 
 					return icon;
 				}
-				if(skillID >= 100)
-					index--;
 			}
 			return nullptr;
 		};
