383c1fef8db12b9314636079dc581eee81bbc555
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2026-04-10 23:11:33 +0200

    Hide turn options controls on in-game scenario info screen

--- client/lobby/OptionsTabBase.cpp
+++ client/lobby/OptionsTabBase.cpp
@@ -445,6 +445,15 @@ void OptionsTabBase::recreate(bool campaign)
 		buttonTurnOptions->block(GAME->server().isGuest() || campaign);
 	}
 
+	if(SEL->screenType == ESelectionScreen::scenarioInfo)
+	{
+		if(auto timerPresetSelector = widget<CIntObject>("timerPresetSelector"))
+			timerPresetSelector->setEnabled(false);
+
+		if(auto buttonTurnOptions = widget<CIntObject>("buttonTurnOptions"))
+			buttonTurnOptions->setEnabled(false);
+	}
+
 	if(auto textureCampaignOverdraw = widget<CFilledTexture>("textureCampaignOverdraw"))
 		textureCampaignOverdraw->setEnabled(campaign);
 }
