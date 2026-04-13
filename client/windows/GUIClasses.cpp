736a14b088981864ba4417d39f1a84330bccfc9a
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2026-04-13 10:27:27 +0200

    Use click-only image order mapping for level-up buttons

--- client/windows/GUIClasses.cpp
+++ client/windows/GUIClasses.cpp
@@ -507,7 +507,7 @@ void CLevelWindow::updateLevelUpData(const CGHeroInstance * hero, PrimarySkill p
 	portrait->addClickCallback(nullptr);
 	portrait->addRClickCallback([hero](){ ENGINE->windows().createAndPushWindow<CRClickPopupInt>(std::make_shared<CHeroWindow>(hero)); });
 	ok = std::make_shared<CButton>(Point(297, 413), AnimationPath::builtin("IOKAY.DEF"), CButton::tooltip(), std::bind(&CLevelWindow::close, this), EShortcut::GLOBAL_ACCEPT);
-	ok->setHoverable(true);
+	ok->setImageOrder(0, 1, 0, 0);
 
 	//%s has gained a level.
 	mainTitle = std::make_shared<CLabel>(192, 33, FONT_MEDIUM, ETextAlignment::CENTER, Colors::WHITE, boost::str(boost::format(LIBRARY->generaltexth->allTexts[444]) % hero->getNameTranslated()));
