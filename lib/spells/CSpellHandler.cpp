d66037474d8c71ec3a2417b23729d8fe69381d3e
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2026-04-14 18:05:18 +0200

    Drop generic spell schema fields for Reinforcements texts

--- lib/spells/CSpellHandler.cpp
+++ lib/spells/CSpellHandler.cpp
@@ -741,14 +741,6 @@ std::shared_ptr<CSpell> CSpellHandler::loadFromJson(const std::string & scope, c
 
 	LIBRARY->generaltexth->registerString(scope, spell->getNameTextID(), json["name"]);
 
-	const std::string baseTextID = TextIdentifier("spell", scope, identifier).get();
-	if(!json["casterInTownText"].String().empty())
-		LIBRARY->generaltexth->registerString(scope, baseTextID + ".casterInTown", json["casterInTownText"]);
-	if(!json["selectTownTitleText"].String().empty())
-		LIBRARY->generaltexth->registerString(scope, baseTextID + ".selectTown.title", json["selectTownTitleText"]);
-	if(!json["selectTownDescriptionText"].String().empty())
-		LIBRARY->generaltexth->registerString(scope, baseTextID + ".selectTown.description", json["selectTownDescriptionText"]);
-
 	logMod->trace("%s: loading spell %s", __FUNCTION__, spell->getNameTranslated());
 
 	for(const auto & schoolJson : json["school"].Struct())
