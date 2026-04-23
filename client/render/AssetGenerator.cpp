f348ed5e259c1867af920e17a00904634f5f8802
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2026-04-23 20:39:29 +0200

    Add status bar darkening overlay for generated dialogs

--- client/render/AssetGenerator.cpp
+++ client/render/AssetGenerator.cpp
@@ -1202,6 +1202,10 @@ AssetGenerator::CanvasPtr AssetGenerator::createDialogBackgroundWithStatusBar(co
 		}
 	}
 
+	// Status bar overlay: darken bottom strip to match original dialog style
+	const int statusBarOverlayHeight = 30;
+	canvas.drawColorBlended(Rect(0, size.y - statusBarOverlayHeight, size.x, statusBarOverlayHeight), ColorRGBA(0, 0, 0, 72));
+
 	auto drawHorizontal = [&canvas](const std::shared_ptr<IImage> & source, int y, int xBegin, int xEnd)
 	{
 		for(int x = xBegin; x < xEnd; x += source->width())
