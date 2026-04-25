6194cf57743abc118a04833aea759ee0058bc115
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2026-04-25 11:04:58 +0200

    Use fixed top coordinates in spell research dialog layout

--- client/windows/CCastleInterface.cpp
+++ client/windows/CCastleInterface.cpp
@@ -86,8 +86,7 @@ CSpellResearchDialog::CSpellResearchDialog(
 	OBJECT_CONSTRUCTION;
 
 	const int sideMargin = 21;
-	const int topMargin = 18;
-	const int titleY = topMargin + 21;
+	const int titleY = 39;
 	const int textTop = 46;
 	const int textHeight = 88;
 	const int statusbarHeight = 26;
