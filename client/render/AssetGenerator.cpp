b812ed8a78ae41f3c7952402a100a1509e3bbdf0
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2026-05-04 19:11:45 +0200

    Align dialog background rows with rendered header+bonus rows

--- client/render/AssetGenerator.cpp
+++ client/render/AssetGenerator.cpp
@@ -94,7 +94,7 @@ void AssetGenerator::initialize()
 	addUniversityConfirmBackground("UNIVRSC1", Point(466, 388), 1);
 	addUniversityConfirmBackground("UNIVRSC2", Point(466, 388), 2);
 	addSpellResearchBackground("spellResearchDialog", Point(328, 474));
-	for(int rowCount = 1; rowCount <= 7; ++rowCount)
+	for(int rowCount = 1; rowCount <= 8; ++rowCount)
 	{
 		const int dialogHeight = 600 - (8 - rowCount) * 36;
 		imageFiles[ImagePath::builtin("stackExperienceDialogRows" + std::to_string(rowCount))] = [this, rowCount, dialogHeight]()
