10f90d531c90696cb0bb8b56bc5e59fee16584e5
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2026-06-05 11:21:35 +0200

    Center reward artifacts on resource background

--- client/widgets/CComponent.h
+++ client/widgets/CComponent.h
@@ -39,10 +39,13 @@ public:
 
 private:
 	std::vector<std::shared_ptr<CLabel>> lines;
+	std::shared_ptr<CAnimImage> artifactOverlay;
 
 	size_t getIndex() const;
 	std::vector<AnimationPath> getFileName() const;
 	void setSurface(const AnimationPath & defName, int imgPos);
+	void setRewardArtifactSurface(const AnimationPath & artifactDefName, int artifactImgPos);
+	bool shouldUseRewardArtifactSurface(ComponentType Type, ESize imageSize) const;
 
 	void init(ComponentType Type, ComponentSubType Subtype, std::optional<int32_t> Val, ESize imageSize, EFonts font, const std::string & ValText);
 
